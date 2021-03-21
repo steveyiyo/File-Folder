bca764bad64773520982f7b4253d0229cf4077ae
steveyiyo <jackcooku@steveyi.net>
2021-03-21 18:08:37 +0800

    Add upload feature

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,8 +15,50 @@ func indexPage(c *gin.Context) {
 	c.HTML(200, "index.html", nil)
 }
 
+func uploadPage(c *gin.Context) {
+	c.HTML(200, "upload.html", nil)
+}
+
 func pageNotAvailable(c *gin.Context) {
 	c.HTML(404, "404.html", nil)
+}
+
+func uploadFile(c *gin.Context) {
+
+	type Result struct {
+		Success   bool
+		Message   string
+		File_Name string
+	}
+
+	var r Result
+
+	file, header, err := c.Request.FormFile("upload_file")
+	if err != nil {
+		r = Result{false, "Bad Request!", ""}
+		c.JSON(400, r)
+		return
+	}
+
+	nsec := time.Now().UnixNano()
+	filename := strconv.FormatInt(nsec, 10) + "_" + header.Filename
+
+	out, err := os.Create("upload_file/" + filename)
+	if err != nil {
+		r = Result{false, "Error!", ""}
+		c.JSON(400, r)
+	}
+
+	defer out.Close()
+
+	_, err = io.Copy(out, file)
+	if err != nil {
+		r = Result{false, "Error!", ""}
+		c.JSON(400, r)
+	} else {
+		r = Result{true, "Upload Success!", filename}
+		c.JSON(201, r)
+	}
 }
 
 func main() {
@@ -30,8 +76,11 @@ func main() {
 	router.LoadHTMLGlob("static/*")
 
 	router.GET("/", indexPage)
-	router.StaticFS("static/", http.Dir("./static"))
+	router.GET("/upload", uploadPage)
+	router.POST("/upload", uploadFile)
+	router.StaticFS("file/", http.Dir("./upload_file"))
 	router.NoRoute(pageNotAvailable)
 
-	router.Run(":13275")
+	router.Run(":80")
+	// router.RunTLS(":443", "certificate/ssl.pem", "certificate/ssl.key")
 }
