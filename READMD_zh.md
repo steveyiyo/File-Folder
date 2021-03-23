3dddb2e676563692a85f06eca05683be3837b327
SteveYi <jackcooku@steveyi.net>
2021-03-23 17:50:40 +0800

    Add README_zh

---
+++ READMD_zh.md
@@ -0,0 +1,26 @@
+# File Folder System
+
+一個可以上傳檔案並下載的系統，就像圖床一樣
+
+### 如何使用？
+
+1. 下載已編譯完成的二進制文件及 [附加檔案](https://github.com/steveyiyo/file_folder/releases/download/v1.0/file_folder.zip)。
+
+![](image/001.png)
+
+2. 將壓縮檔解壓縮為資料夾，並將編譯完成的二進制文件複製進去。
+
+3. 執行二進制文件！
+
+**附帶說明：您可以在 static 目錄中編輯網頁。**
+**請注意不要占用到 80 Port，否則程式會關閉**
+
+### 截圖
+
+![](image/002.png)
+
+![](image/003.png)
+
+![](image/004.png)
+
+![](image/001.png)
--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 echo "Starting for build!\n"
 
 echo "Linux amd64\n"
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/Linux_arm64
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/Linux_amd64
 echo "macOS amd64\m"
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o build/macOS_arm64
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o build/macOS_amd64
 echo "Windows amd64\n"
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build/Windows_arm64.exe
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build/Windows_amd64.exe
