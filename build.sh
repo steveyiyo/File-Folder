7414b519f1fab1d4fd3a52a1944a0cd375d95507
SteveYi <jackcooku@steveyi.net>
2021-04-03 03:09:54 +0800

    fix type

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 echo "Starting for build!\n"
 
-echo "Linux amd64\n"
+echo "Linux amd64"
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/Linux_Intel64
-echo "macOS amd64\m"
+echo "macOS amd64"
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o build/macOS_Intel64
-echo "Windows amd64\n"
+echo "Windows amd64"
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build/Windows_Intel64.exe
